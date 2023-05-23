4945c8f014eac48200112f84be33f4b609323935
Mauricio-tech <mauricio.trevinon91@gmail.com>
2023-05-23 14:15:54 -0600

    Added comment, index and post model

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User associations
+User.hasMany(Post, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+// Post associations
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+// Comment associations
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
